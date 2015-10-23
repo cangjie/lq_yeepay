d1ec99c20adb36e5517642a21022f51f47f50426
cangjie <cangjie@hotmail.com>
2015-10-23 10:55:07 +0800

    mod order number to test

--- App_Code/Test/QueryPayResultTest.cs
+++ App_Code/Test/QueryPayResultTest.cs
@@ -11,7 +11,7 @@ public    class QueryPayResultTest
         {
             
             //商户交易订单号
-            string orderid = "1443583781000697";
+            string orderid = "1444557850000701";
 
             YJPay yjpay = new YJPay();
             //调用sdk请求一键支付接口
